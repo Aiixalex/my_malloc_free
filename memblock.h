d269928dcc7a67afc9bc2663a19e4e468df4adc5
Yilin 'Alex' Wang <redcloveralex@gmail.com>
2021-11-11 00:25:55 -0800

    feat: add struct memblock_region, memblock_type, memblock

--- memblock.h
+++ memblock.h
@@ -1,3 +1,20 @@
-struct memblock {
+typedef unsigned long long phys_addr_t;
 
+struct memblock_region {
+    phys_addr_t base;
+    phys_addr_t size;
+
+};
+
+struct memblock_type {
+    unsigned long cnt;	/* number of regions */
+	unsigned long max;	/* size of the allocated array */
+	phys_addr_t total_size;	/* size of all regions */
+	struct memblock_region *regions;
+	char *name;
+};
+
+struct memblock {
+    struct memblock_type memory;
+	struct memblock_type reserved;
 };
